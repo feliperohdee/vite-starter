17b1e2fbf8c66e10e75bf437ce321b52a253d665
Felipe Rohde <feliperohdee@gmail.com>
2025-02-22 11:06:24 -0300

    Add welcome page with installation instructions and project overview

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import './index.css';
 import { createRoot } from 'react-dom/client';
 import { StrictMode } from 'react';
 import { Toaster } from '@/components/ui/toaster';
-import App from './App.tsx';
+import App from '@/app';
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
